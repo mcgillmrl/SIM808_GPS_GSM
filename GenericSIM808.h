74a3d696e9ceacf94c73f5680af5c1b7bb3063fb
Juan Camilo Gamboa Higuera <juancamilog@gmail.com>
2019-11-15 02:28:54 -0500

    using FONA library now

---
+++ GenericSIM808.h
@@ -0,0 +1,131 @@
+#include "Adafruit_FONA.h"
+
+
+class GenericSIM808 : public Adafruit_FONA{
+  public:
+    GenericSIM808(int8_t r):Adafruit_FONA(r){};
+    
+    boolean begin_(Stream &port){
+      mySerial = &port;
+
+    // first check if on
+    int16_t timeout = 3000;
+    bool device_on = false;
+    while (timeout > 0) {
+      while (mySerial->available()) mySerial->read();
+      device_on = sendCheckReply(F("AT"), ok_reply);
+      if (device_on)
+        break;
+      while (mySerial->available()) mySerial->read();
+      device_on = sendCheckReply(F("AT"), F("AT"));
+      if (device_on)
+        break;
+      delay(500);
+      timeout-=500;
+    }
+    
+    pinMode(_rstpin, OUTPUT);
+    if(device_on){
+      SerialUSB.println(F("Device is on, powering off"));
+      digitalWrite(_rstpin, HIGH);
+      delay(2000);
+      digitalWrite(_rstpin, LOW);
+      delay(1000); 
+    }
+    SerialUSB.println(F("Powering device on"));
+    digitalWrite(_rstpin, HIGH);
+    delay(2000);
+    digitalWrite(_rstpin, LOW);
+    delay(1000);
+  
+    DEBUG_PRINTLN(F("Attempting to open comm with ATs"));
+    // give 7 seconds to reboot
+    timeout = 7000;
+  
+    while (timeout > 0) {
+      while (mySerial->available()) mySerial->read();
+      if (sendCheckReply(F("AT"), ok_reply))
+        break;
+      while (mySerial->available()) mySerial->read();
+      if (sendCheckReply(F("AT"), F("AT")))
+        break;
+      delay(500);
+      timeout-=500;
+    }
+  
+    if (timeout <= 0) {
+  #ifdef ADAFRUIT_FONA_DEBUG
+      DEBUG_PRINTLN(F("Timeout: No response to AT... last ditch attempt."));
+  #endif
+      sendCheckReply(F("AT"), ok_reply);
+      delay(100);
+      sendCheckReply(F("AT"), ok_reply);
+      delay(100);
+      sendCheckReply(F("AT"), ok_reply);
+      delay(100);
+    }
+  
+    // turn off Echo!
+    sendCheckReply(F("ATE0"), ok_reply);
+    delay(100);
+  
+    if (! sendCheckReply(F("ATE0"), ok_reply)) {
+      return false;
+    }
+ 
+    // set error messages to be verbose
+    sendCheckReply(F("AT+CMEE=2"), ok_reply);
+    
+    // turn on hangupitude
+    sendCheckReply(F("AT+CVHU=0"), ok_reply);
+  
+    delay(100);
+    flushInput();
+  
+  
+    DEBUG_PRINT(F("\t---> ")); DEBUG_PRINTLN("ATI");
+  
+    mySerial->println("ATI");
+    readline(500, true);
+  
+    DEBUG_PRINT (F("\t<--- ")); DEBUG_PRINTLN(replybuffer);
+  
+  
+  
+    if (prog_char_strstr(replybuffer, (prog_char *)F("SIM808 R14")) != 0) {
+      _type = FONA808_V2;
+    } else if (prog_char_strstr(replybuffer, (prog_char *)F("SIM808 R13")) != 0) {
+      _type = FONA808_V1;
+    } else if (prog_char_strstr(replybuffer, (prog_char *)F("SIM800 R13")) != 0) {
+      _type = FONA800L;
+    } else if (prog_char_strstr(replybuffer, (prog_char *)F("SIMCOM_SIM5320A")) != 0) {
+      _type = FONA3G_A;
+    } else if (prog_char_strstr(replybuffer, (prog_char *)F("SIMCOM_SIM5320E")) != 0) {
+      _type = FONA3G_E;
+    }
+  
+    if (_type == FONA800L) {
+      // determine if L or H
+  
+    DEBUG_PRINT(F("\t---> ")); DEBUG_PRINTLN("AT+GMM");
+  
+      mySerial->println("AT+GMM");
+      readline(500, true);
+  
+    DEBUG_PRINT (F("\t<--- ")); DEBUG_PRINTLN(replybuffer);
+  
+  
+      if (prog_char_strstr(replybuffer, (prog_char *)F("SIM800H")) != 0) {
+        _type = FONA800H;
+      }
+    }
+  
+  #if defined(FONA_PREF_SMS_STORAGE)
+      sendCheckReply(F("AT+CPMS=" FONA_PREF_SMS_STORAGE "," FONA_PREF_SMS_STORAGE "," FONA_PREF_SMS_STORAGE), ok_reply);
+  #endif
+  
+    return true;  
+  };
+  
+};
+
--- SIM808_GPS_GSM.ino
+++ SIM808_GPS_GSM.ino
@@ -1,8 +1,22 @@
 #include<stdio.h>
 #include<string.h>
+#define ADAFRUIT_FONA_DEBUG
+#include "GenericSIM808.h"
 #define DEBUG true
-int pon=9;
+
+
+int power_on=9;
+char replybuffer[250];
+
+// Use this for FONA 800 and 808s
+GenericSIM808 sim808 = GenericSIM808(power_on);
+uint8_t type;
+
 //gps data;
+bool gps_forwarding = false;
+int gps_msg_count = 0;
+char gpsdata[120];
+long last_msg_time=millis();
 int stat=0;
 float timestamp=20191111191056.000;
 float lat=45.534430;
@@ -22,57 +36,135 @@ int CN0_max=0;
 void setup()
 {
   SerialUSB.begin(1000000);
-  //while (!SerialUSB) {
-  //  ; // wait for serial port to connect
-  //}
-  Serial1.begin(57600);
-  delay(1000);
-  reset();
-  delay(1000);
-  reset();
-  SerialUSB.println("Waiting for 3 seconds to boot up");
-  delay(3000);
-  SerialUSB.println("Initializing GSM");
-  gsm_connect();
-  SerialUSB.println("Initializing GPS");
-  gps_power_on();
-  sendData("AT+CNTP?",3000,DEBUG);
+  while (!SerialUSB) {
+    ; // wait for serial port to connect
+  }
+  sim808_init();
 }
 
 void loop()
 {
-    // read GPS
-    String response = sendData( "AT+CGNSINF",1000,DEBUG);
-    process_gps_msg((char*) response.c_str());
-    send_http_get_request();
+    // check gps status
+    checkGPS();
+
+    if(sim808.available()){
+      while (sim808.available()) {
+         SerialUSB.write(sim808.read());
+      }
+    }
+             
+    if ((gps_msg_count % 30) == 1){
+      SerialUSB.println("Forwarding GPS data to remote server");
+      if (gps_forwarding)
+        sim808.enableGPSNMEA(0);
+      send_http_get_request();
+      if (gps_forwarding)
+        sim808.enableGPSNMEA(1);
+    }
 }
 
-void reset(){
-  pinMode(pon, OUTPUT);
-  digitalWrite(pon, HIGH);
+
+void sim808_init(){
+  Serial1.begin(115200);
+  if (!sim808.begin_(Serial1)) {
+    SerialUSB.println(F("Couldn't find SIM808"));
+    while (1);
+  }
+  delay(1000);
+  
+  // Print module IMEI number.
+  char imei[16] = {0}; // MUST use a 16 character buffer for IMEI!
+  uint8_t imeiLen = sim808.getIMEI(imei);
+  if (imeiLen > 0) {
+    SerialUSB.print(F("Module IMEI: ")); SerialUSB.println(imei);
+  }
+
+  gsm_connect();
+  gps_power_on();
+}
+
+void switch_power(){
+  pinMode(power_on, OUTPUT);
+  digitalWrite(power_on, HIGH);
   delay(3000);
-  digitalWrite(pon, LOW);
+  digitalWrite(power_on, LOW);
   delay(1000); 
 }
 
 bool gps_power_on(){
-     sendData( "AT+CGNSPWR=1",1000,DEBUG);
-     sendData("AT+CGNSSEQ=RMC",1000,DEBUG);
+  // turn gps on
+  SerialUSB.print("Turning GPS on...");
+  while(!sim808.enableGPS(true)){
+    delay(1000);
+  }
+  SerialUSB.println(" OK");
+  delay(1000);
 }
 
 bool gsm_connect(){
-     SerialUSB.println("Checking network registration");
-     sendData("AT+CREG?",3000,DEBUG);     
-     SerialUSB.println("Attaching to GSM service");
-     sendData("AT+CMGF=1",1000,DEBUG);
-     sendData("AT+CGATT=1",1000,DEBUG);
-     SerialUSB.println("Setting APN to hologram");
-     sendData("AT+CSTT=hologram",3000,DEBUG);
-     SerialUSB.println("Bring up wireless connection");
-     sendData("AT+CIICR",3000,DEBUG);
-     SerialUSB.println("Getting currently assigned IP");
-     sendData("AT+CIFSR",3000,DEBUG);
-     
+  // set GPRS network settings
+  sim808.setGPRSNetworkSettings(F("hologram"), F(""), F(""));
+
+  // wait until network is up
+  SerialUSB.print(F("Waiting for network registration..."));
+  uint8_t n = sim808.getNetworkStatus();
+  while (n != 1 && n != 5){
+    delay(1000);
+    n = sim808.getNetworkStatus();
+  }
+  SerialUSB.println(" OK");
+
+  // wait until gprs is successfully enabled
+  SerialUSB.print(F("Enabling GPRS..."));
+  while(!sim808.enableGPRS(true)){
+    delay(1000);
+  }
+  SerialUSB.println(F(" OK"));
+}
+
+void checkGPS(){
+  if(millis() - last_msg_time > 1000){
+    int8_t stat = sim808.GPSstatus(); 
+    switch (stat){
+      case 0:
+        gps_power_on(); 
+        break;
+      case 1:
+        if (gps_forwarding){
+          SerialUSB.println("Disabled GPS forwarding");
+          sim808.enableGPSNMEA(0);
+          gps_forwarding = false;
+        }
+        delay(1000);
+      case 2:
+      case 3:
+        // start forwarding
+        if (!gps_forwarding){
+          SerialUSB.println("Forwarding GPS data to host");
+          sim808.enableGPSNMEA(1);
+          gps_forwarding = true;
+        }
+        // read gps
+        last_msg_time = millis();
+        sim808.getGPS(0, gpsdata, 120);
+        process_gps_msg(gpsdata);
+        gps_msg_count++;
+        break;
+      default:
+        break;
+    }
+  }
+}
+
+uint16_t GSMLoc(){
+  uint16_t returncode;
+  while(!sim808.getGSMLoc(&returncode, replybuffer, 250)){
+    delay(1000);
+  }
+  SerialUSB.print("Return code: ");
+  SerialUSB.println(returncode);
+  SerialUSB.println(replybuffer);
+  return returncode;
 }
 
 void process_gps_msg(char* data){
@@ -80,33 +172,33 @@ void process_gps_msg(char* data){
   int i = 0;
   while(ptr != NULL){
     switch(i){
-      case 2:
+      case 1:
           stat = atoi(ptr);
-      case 3:
+      case 2:
           timestamp = atof(ptr);
-      case 4:
+      case 3:
           lat = atof(ptr);
-      case 5:
+      case 4:
           lon = atof(ptr);
-      case 6:
+      case 5:
           alt = atof(ptr);
-      case 7:
+      case 6:
           speed_over_ground = atof(ptr);
-      case 8:
+      case 7:
           course_over_ground = atof(ptr);
-      case 9:
+      case 8:
           fix_mode = atoi(ptr);
-      case 10:
+      case 9:
           hdop = atof(ptr);
-      case 11:
+      case 10:
           pdop = atof(ptr);
-      case 12:
+      case 11:
           vdop = atof(ptr);
-      case 13:
+      case 12:
           sat_in_view = atoi(ptr);
-      case 14:
+      case 13:
           sat_used = atoi(ptr);
-      case 15:
+      case 14:
           CN0_max = atoi(ptr);
     }
     i++;
@@ -114,45 +206,37 @@ void process_gps_msg(char* data){
   }
 }
 
-bool send_http_get_request(){
-  sendData("AT+HTTPINIT",1000,DEBUG);
-  sendData("AT+HTTPPARA=\"CID\",1",1000,DEBUG);
-  sendData("AT+HTTPPARA=\"UA\",\"AQUA5\"",1000,DEBUG);
-  // http request
-  Serial1.print("AT+HTTPPARA=");
-  Serial1.print("\"URL\",\"");
-  Serial1.print("http://132.206.74.128:8000"); // URL of location server
-  Serial1.print("?lat=");
-  Serial1.print(lat);
-  Serial1.print("&lon=");
-  Serial1.print(lon);
-  Serial1.print("&alt=");
-  Serial1.print(alt);
-  Serial1.print("&time=");
-  Serial1.print(timestamp);
-  sendData("\"",1000,DEBUG);
-  sendData("AT+HTTPACTION=0",1000,DEBUG);
-  sendData("AT+HTTPREAD",1000,DEBUG);
-  sendData("AT+HTTPCLOSE",1000,DEBUG);
+void send_http_get_request(){
+  uint16_t statuscode;
+  uint16_t datalen = 0;
+  String url = F("http://132.206.74.128:8000");
+  url += "?lat=";
+  url += lat;
+  url += "&lon=";
+  url += lon;
+  url += "&timestamp=";
+  url += timestamp;
+  if (!sim808.HTTP_GET_start((char *)url.c_str(), &statuscode, &datalen)){
+    SerialUSB.print(F("GET request failed. Status: "));
+    SerialUSB.println(statuscode);
+  }
+  SerialUSB.println(F("\n****\n"));
+  while (datalen > 0) {
+    while (sim808.available()) {
+      char c = sim808.read();
+
+#if defined(__AVR_ATmega328P__) || defined(__AVR_ATmega168__)
+      loop_until_bit_is_set(UCSR0A, UDRE0); /* Wait until data register empty. */
+      UDR0 = c;
+#else
+      SerialUSB.write(c);
+#endif
+      datalen--;
+      if (!datalen) 
+        break;
+    }
+  }
+  SerialUSB.println(F("\n****\n"));
+  sim808.HTTP_POST_end();
 }
 
-
-String sendData(String command, const int timeout, boolean debug)
-{
-    String response = "";    
-    Serial1.println(command); 
-    long int time = millis();   
-    while( (time+timeout) > millis())
-    {
-      while(Serial1.available())
-      {       
-        char c = Serial1.read(); 
-        response+=c;
-      }  
-    }    
-    if(debug)
-    {
-      SerialUSB.print(response);
-    }    
-    return response;
-}
